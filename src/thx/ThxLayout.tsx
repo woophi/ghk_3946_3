64937c2da7d30e235bd4a38a03c0d88d9f14b980
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-12-13 07:25:01 +0100

    init

---
+++ src/thx/ThxLayout.tsx
@@ -0,0 +1,37 @@
+import { ButtonMobile } from '@alfalab/core-components/button/mobile';
+import { CDNIcon } from '@alfalab/core-components/cdn-icon';
+import { Typography } from '@alfalab/core-components/typography';
+import { appSt } from '../style.css';
+import { thxSt } from './style.css';
+
+export const ThxLayout = ({ selectedEns }: { selectedEns: boolean }) => {
+  return (
+    <>
+      <div className={thxSt.container}>
+        <div className={thxSt.rocket}>
+          <CDNIcon className={thxSt.icon} name={selectedEns ? 'glyph_screwdriver-paint-brush_m' : 'glyph_briefcase_m'} />
+        </div>
+
+        <Typography.TitleResponsive font="system" tag="h1" view="large" defaultMargins weight="bold">
+          {selectedEns
+            ? 'Страховка позиции по данному инструменту не доступна'
+            : 'Попробуйте подать торговое поручение иным способом'}
+        </Typography.TitleResponsive>
+        <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
+          Попробуйте подать заявку без страхования позиции
+        </Typography.Text>
+      </div>
+
+      <div className={appSt.bottomBtn}>
+        <ButtonMobile
+          block
+          view="primary"
+          href="alfabank://"
+          onClick={() => window.gtag('event', 'Buy_no_insurance_3946_click_var3')}
+        >
+          Подать заявку без страхования
+        </ButtonMobile>
+      </div>
+    </>
+  );
+};
---
+++ src/utils/events.ts
@@ -0,0 +1,38 @@
+declare global {
+  interface Window {
+    dataLayer: unknown[];
+    gtag: (e: 'event', v: string) => void;
+  }
+}
+
+type Payload = {
+  quantity: number;
+  price: number;
+  term: number | 'Nan';
+  percent: number | 'Nan';
+  percent_down: number | 'Nan';
+  cost: number;
+};
+
+export const sendDataToGA = async (payload: Payload) => {
+  try {
+    const now = new Date();
+    const date = `${now.getFullYear()}-${
+      now.getMonth() + 1
+    }-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+    await fetch(
+      'https://script.google.com/macros/s/AKfycbxMcsAckqE9-NtAoZZMGDjQ7Ce3rKoos6aQcHew4dqH7UnENnaFANj2gA9OnsNSJJV1/exec',
+      {
+        redirect: 'follow',
+        method: 'POST',
+        body: JSON.stringify({ date, ...payload, variant: 'var3' }),
+        headers: {
+          'Content-Type': 'text/plain;charset=utf-8',
+        },
+      },
+    );
+  } catch (error) {
+    console.error('Error!', error);
+  }
+};
